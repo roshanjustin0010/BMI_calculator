cbabc882ddf77872a513590e05b6c8f2ea5d75d7
roshan <roshanjustin0010@gmail.com>
2022-12-06 23:21:10 +0530

    version0.2

--- lib/Home_page.dart
+++ lib/Home_page.dart
@@ -10,35 +10,32 @@ class Home_pg extends StatefulWidget {
 }
 
 class _Home_pgState extends State<Home_pg> {
-  int _weight=50;
-  int _age=15;
-  int _height=150;
+  int _weight = 50;
+  int _age = 15;
+  int _height = 150;
 
-
-  void _addweight(){
+  void _addweight() {
     setState(() {
       _weight++;
     });
-
   }
-  void _subweight(){
+
+  void _subweight() {
     setState(() {
       _weight--;
     });
-
   }
 
-  void _addage(){
+  void _addage() {
     setState(() {
       _age++;
     });
-
   }
-  void _subage(){
+
+  void _subage() {
     setState(() {
       _age--;
     });
-
   }
 
   //result=(_weight/_height/_height)*10000;
@@ -47,12 +44,12 @@ class _Home_pgState extends State<Home_pg> {
     return MaterialApp(
       theme: ThemeData.dark(),
       home: Scaffold(
-        appBar: AppBar(
-          title: Center(child: Text('BMI Calculator'))
-        ),
+        appBar: AppBar(title: Center(child: Text('BMI Calculator'))),
         body: Column(
           children: [
-            SizedBox(height: 25,),
+            SizedBox(
+              height: 25,
+            ),
 
             //gender
             Row(
@@ -65,7 +62,7 @@ class _Home_pgState extends State<Home_pg> {
                     height: 150,
                     width: 150,
                     color: Color(0xFF0A0E35),
-                    child:Column(
+                    child: Column(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
                         Icon(
@@ -73,19 +70,21 @@ class _Home_pgState extends State<Home_pg> {
                           size: 75,
                           semanticLabel: 'male',
                         ),
-                        Text('Male',style: TextStyle(fontSize: 30),)
+                        Text(
+                          'Male',
+                          style: TextStyle(fontSize: 30),
+                        )
                       ],
                     ),
                   ),
                 ),
-
 
                 //female box
                 Container(
                   height: 150,
                   width: 150,
                   color: Color(0xFF0A0E35),
-                  child:Column(
+                  child: Column(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
                       Icon(
@@ -93,7 +92,10 @@ class _Home_pgState extends State<Home_pg> {
                         size: 75,
                         semanticLabel: 'Female',
                       ),
-                      Text('Female',style: TextStyle(fontSize: 30),)
+                      Text(
+                        'Female',
+                        style: TextStyle(fontSize: 30),
+                      )
                     ],
                   ),
                 ),
@@ -101,7 +103,9 @@ class _Home_pgState extends State<Home_pg> {
             ),
             //end of genderbox
 
-            SizedBox(height: 25,),
+            SizedBox(
+              height: 25,
+            ),
 
             //height slider
 
@@ -113,9 +117,15 @@ class _Home_pgState extends State<Home_pg> {
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
-                    Text('Height',style: TextStyle(fontSize: 30),),
+                    Text(
+                      'Height',
+                      style: TextStyle(fontSize: 30),
+                    ),
 
-                    Text('$_height cm',style: TextStyle(fontSize: 30),),
+                    Text(
+                      '$_height cm',
+                      style: TextStyle(fontSize: 30),
+                    ),
 
                     //slider
 
@@ -125,31 +135,33 @@ class _Home_pgState extends State<Home_pg> {
                           inactiveTrackColor: Colors.white,
                           thumbColor: Color(0xFFE88100),
                           overlayColor: Color(0xFF7a7a7a),
-                          thumbShape: RoundSliderThumbShape(enabledThumbRadius: 18),
-                          overlayShape: RoundSliderOverlayShape(overlayRadius: 25)
-                      ),
+                          thumbShape:
+                              RoundSliderThumbShape(enabledThumbRadius: 18),
+                          overlayShape:
+                              RoundSliderOverlayShape(overlayRadius: 25)),
                       child: Slider(
-                          value: _height.toDouble(),
-                           min: 120,
-                            max: 220,
-                          // activeColor: Color(0xff027b8d),
-                          onChanged: (double newVal){
-                            setState(() {
-                              _height=newVal.toInt();
-                            });
-                          },
+                        value: _height.toDouble(),
+                        min: 120,
+                        max: 220,
+                        // activeColor: Color(0xff027b8d),
+                        onChanged: (double newVal) {
+                          setState(() {
+                            _height = newVal.toInt();
+                          });
+                        },
                       ),
                     ),
 
                     //end slider
-
                   ],
                 ),
               ),
             ),
             //end of height slider
 
-            SizedBox(height: 25,),
+            SizedBox(
+              height: 25,
+            ),
 
             //weight age box
 
@@ -161,33 +173,39 @@ class _Home_pgState extends State<Home_pg> {
                   height: 150,
                   width: 150,
                   color: Color(0xFF0A0E35),
-                  child:Column(
+                  child: Column(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
-                      Text('Weight',style: TextStyle(fontSize: 20),),
-                      
-                      Text('$_weight',style: TextStyle(fontSize: 30),),
+                      Text(
+                        'Weight',
+                        style: TextStyle(fontSize: 20),
+                      ),
+
+                      Text(
+                        '$_weight',
+                        style: TextStyle(fontSize: 30),
+                      ),
 
                       //+ - button
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceAround,
                         children: [
-                          FloatingActionButton(onPressed: _subweight,
-                            backgroundColor:Color(0xFFE88100),
+                          FloatingActionButton(
+                            onPressed: _subweight,
+                            backgroundColor: Color(0xFFE88100),
                             child: Icon(Icons.minimize),
-                            // heroTag: "btn3",
+                            heroTag: "btn3",
                           ),
-
-                          FloatingActionButton(onPressed: _addweight,
-                            backgroundColor:Color(0xFFE88100),
+                          FloatingActionButton(
+                            onPressed: _addweight,
+                            backgroundColor: Color(0xFFE88100),
                             child: Icon(Icons.add),
-                            // heroTag: "btn4",
+                            heroTag: "btn4",
                           )
                         ],
                       ),
 
                       //end + - button
-
                     ],
                   ),
                 ),
@@ -197,27 +215,34 @@ class _Home_pgState extends State<Home_pg> {
                   height: 150,
                   width: 150,
                   color: Color(0xFF0A0E35),
-                  child:Column(
+                  child: Column(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
-                      Text('Age',style: TextStyle(fontSize: 20),),
+                      Text(
+                        'Age',
+                        style: TextStyle(fontSize: 20),
+                      ),
 
-                      Text('$_age',style: TextStyle(fontSize: 30),),
+                      Text(
+                        '$_age',
+                        style: TextStyle(fontSize: 30),
+                      ),
 
                       //+ - button
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceAround,
                         children: [
-                          FloatingActionButton(onPressed: _subage,
-                            backgroundColor:Color(0xFFE88100),
+                          FloatingActionButton(
+                            onPressed: _subage,
+                            backgroundColor: Color(0xFFE88100),
                             child: Icon(Icons.minimize),
-                            // heroTag: "btn1",
+                            heroTag: "btn1",
                           ),
-
-                          FloatingActionButton(onPressed: _addage,
-                            backgroundColor:Color(0xFFE88100),
+                          FloatingActionButton(
+                            onPressed: _addage,
+                            backgroundColor: Color(0xFFE88100),
                             child: Icon(Icons.add),
-                            // heroTag: "btn2",
+                            heroTag: "btn2",
                           )
                         ],
                       ),
@@ -230,43 +255,43 @@ class _Home_pgState extends State<Home_pg> {
             ),
 
             //end weight age box
-            
-            SizedBox(height: 25,),
-            
+
+            SizedBox(
+              height: 25,
+            ),
+
             //calculate button
 
             GestureDetector(
-              onTap: (){
-                Calculatorbrain calc= Calculatorbrain(height: _height,weight: _weight);
-                var calbmi=calc.caluculatebmi();
-                var stage =calc.getresult();
-                var txt=calc.gettext();
-                var bgcol=calc.getcolor();
-                var txtcolr=calc.txtcolor();
+              onTap: () {
+                Calculatorbrain calc =
+                    Calculatorbrain(height: _height, weight: _weight);
+                var calbmi = calc.caluculatebmi();
+                var stage = calc.getresult();
+                var txt = calc.gettext();
+                var bgcol = calc.getcolor();
+                var txtcolr = calc.txtcolor();
 
-            Navigator.of(context).push(MaterialPageRoute(builder: (context)=>Result_pg(
-              getcol: bgcol,
-              stg: stage,
-              gettxt: txt,
-              getcal: calbmi,
-              txtcol: txtcolr,
-            )));
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => Result_pg(
+                          getcol: bgcol,
+                          stg: stage,
+                          gettxt: txt,
+                          getcal: calbmi,
+                          txtcol: txtcolr,
+                        )));
               },
               child: Container(
                 height: 50,
                 width: 300,
-                color:Color(0xFFE88100),
-                child: Center(child: Text('Calculate',
-                  style: TextStyle(fontSize: 30,color: Colors.black),
-
-                )
-                ),
+                color: Color(0xFFE88100),
+                child: Center(
+                    child: Text(
+                  'Calculate',
+                  style: TextStyle(fontSize: 30, color: Colors.black),
+                )),
               ),
             ),
-
-
-
-
           ],
         ),
       ),
